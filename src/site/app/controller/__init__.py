c18c032ccd05061d121b27d46675a57513adc917
AlanOndra <alan@aondra.com>
2014-10-29 23:53:44 -0400

    Update __init__.py

--- src/site/app/controller/__init__.py
+++ src/site/app/controller/__init__.py
@@ -4,15 +4,6 @@ from os import listdir
 from os.path import abspath, dirname, isfile, join
 
 
-'''
-cwd = abspath(join(dirname(__file__), '..'))
-
-__all__ = [str(f)[:-3] for f in listdir(cwd) if isfile(join(cwd, f)) and f.endswith('.py')]
-'''
-
 __all__ = [
-	'index',
-	'lang',
-	'blog',
-	'static'
+	'index'
 ]
